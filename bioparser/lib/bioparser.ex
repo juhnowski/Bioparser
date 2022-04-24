9eacd8f539023543aefae620ac95965afa057a89
Илья Юхновский <ilya@MacBook-Pro-Ila.local>
2022-04-24 15:58:37 +0300

    first commit

---
+++ bioparser/lib/bioparser.ex
@@ -0,0 +1,18 @@
+defmodule Bioparser do
+  @moduledoc """
+  Documentation for `Bioparser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bioparser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ bioparser/test/bioparser_test.exs
@@ -0,0 +1,8 @@
+defmodule BioparserTest do
+  use ExUnit.Case
+  doctest Bioparser
+
+  test "greets the world" do
+    assert Bioparser.hello() == :world
+  end
+end
