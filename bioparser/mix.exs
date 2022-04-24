03d68f726f83a3704566c600047c5854fd6ad404
Илья Юхновский <ilya@MacBook-Pro-Ila.local>
2022-04-25 02:11:23 +0300

    add doc

--- bioparser/mix.exs
+++ bioparser/mix.exs
@@ -6,6 +6,8 @@ defmodule Bioparser.MixProject do
       app: :bioparser,
       escript: escript_config(),
       version: "0.1.0",
+      name: "Bioparser",
+      source_url: "https://github.com/juhnowski/Bioparser",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -23,7 +25,9 @@ defmodule Bioparser.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.8"},
-      {:poison, "~> 5.0"}
+      {:poison, "~> 5.0"},
+      {:ex_doc, "~> 0.28.3"},
+      {:earmark, "~> 1.4"},
     ]
   end
 
